5e7aaa642b812a695844e167d0f42f9e09c98aef
Nevss <mr.nevss@gmail.com>
2022-04-15 22:22:59 +0700

    day10

---
+++ day10/part1.py
@@ -0,0 +1,22 @@
+def look_and_say(n):
+    i = 0
+    resultList = []
+    while i < len(n):
+        count = 1
+        while i + 1 < len(n) and n[i] == n[i + 1]:
+            count += 1
+            i += 1
+        resultList.append(str(count) + n[i])
+        i += 1
+
+    return (''.join(resultList))
+
+
+with open('input.txt', 'r') as f:
+    item = f.readline()
+
+for _ in range(40):
+    item = look_and_say(item)
+
+with open('output1.txt', 'w') as f:
+    print(len(str(item)), file=f)
